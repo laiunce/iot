201d0a51947303605a72e3eba4e70b64fb1ac6e1
cristian.laiun <cristian.laiun@publicisgroupe.com>
2019-06-16 23:54:19 -0300

    adding decoder logic

--- src/main/java/com/iot/iot/repository/TrackingDaoImpl.java
+++ src/main/java/com/iot/iot/repository/TrackingDaoImpl.java
@@ -3,11 +3,15 @@ package com.iot.iot.repository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -15,6 +19,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static javax.xml.bind.DatatypeConverter.*;
 
 
 /**
@@ -57,6 +63,20 @@ public class TrackingDaoImpl implements TrackingDao {
         }
     }
 
+    private String hexDecoder(String messageEncode) {
+        String decoded = "";
+        try {
+            byte[] bytes = Hex.decodeHex(messageEncode.toCharArray());
+            decoded = new String(bytes, "UTF-8");
+        } catch (DecoderException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return decoded;
+
+    }
+
     private String getJsonString(String imei, String momsn, String transmit_time, String iridium_latitude, String iridium_longitude, String iridium_cep, String data) {
 
         Map<String, Object> jsonMap = new LinkedHashMap<>();
@@ -66,7 +86,7 @@ public class TrackingDaoImpl implements TrackingDao {
         jsonMap.put("iridium_latitude", iridium_latitude);
         jsonMap.put("iridium_longitude", iridium_longitude);
         jsonMap.put("iridium_cep", iridium_cep);
-        jsonMap.put("data", data);
+        jsonMap.put("dataDecoded", hexDecoder(data));
         try {
             return objectMapper.writeValueAsString(jsonMap);
         } catch (JsonProcessingException ignore) {
